ac2a724f58195743b872fd386af9c398b0669774
mr330136 <mr330136@outlook.com>
2020-05-09 16:40:07 +0530

    Added Repositories

---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WedAp_Core.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [MaxLength(20)]
+        public string Email { get; set; }
+    }
+}
